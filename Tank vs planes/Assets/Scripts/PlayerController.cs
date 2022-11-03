f7235e2b2fb787cc5ebe72bb4d15682e50ab25c1
H7H5 <Serdyk87@ukr.net>
2022-11-03 17:33:55 +0200

    Animation base Tank

--- Tank vs planes/Assets/Scripts/PlayerController.cs
+++ Tank vs planes/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 0.001f;
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void FixedUpdate()
     {
+        float StartX = transform.position.x;
         Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 EndPoint = new Vector3(worldMousePosition.x, transform.position.y, 0f);
-
         transform.position = Vector3.Lerp(transform.position, EndPoint, (Time.deltaTime * Speed)/4);
+        SetAnimation(StartX, transform.position.x);
+    }
+
+    private void SetAnimation(float StartX, float EndX)
+    {
+        float deltaX = Mathf.Abs(StartX - EndX);
+        if(StartX > EndX)
+        {
+            if (deltaX < 0.01f)
+            {
+                animator.SetInteger("direction", 0);
+            }
+            else
+            {
+                animator.SetInteger("direction", -1);
+            }
+        }
+        else
+        {
+            if (deltaX < 0.1f)
+            {
+                animator.SetInteger("direction", 0);
+            }
+            else
+            {
+                animator.SetInteger("direction", 1);
+            }
+        }
     }
 }
