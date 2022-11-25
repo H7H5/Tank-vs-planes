ddb4cddae5f0d41be9dbc20db91a4fc2281c4d39
H7H5 <Serdyk87@ukr.net>
2022-11-25 13:52:52 +0200

    GuidanceMissile

--- Tank vs planes/Assets/Scripts/BoScripts/Homing.cs
+++ Tank vs planes/Assets/Scripts/BoScripts/Homing.cs
@@ -7,13 +7,30 @@ public class Homing : MonoBehaviour
     [SerializeField] private List<Sprite> spritesHoming = new List<Sprite>();
     [SerializeField] private SpriteRenderer spriteRenderer;
     private int level = 0;
+
+    private float[] cooldown = { 0.5f, 0.4f, 0.25f };
+    [SerializeField] GameObject bullet;
+
+    private float timeRtwShots;
+    public float startTimeRtwShots = 0;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-        
+        if (timeRtwShots <= 0 && startTimeRtwShots > 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Shot();
+                timeRtwShots = startTimeRtwShots;
+            }
+        }
+        else
+        {
+            timeRtwShots -= Time.deltaTime;
+        }
     }
 
     public void UpdeteHoming()
@@ -24,5 +41,13 @@ public class Homing : MonoBehaviour
             level = 3;
         }
         spriteRenderer.sprite = spritesHoming[level-1];
+        startTimeRtwShots = cooldown[level-1];
     }
-}
+    private void Shot()
+    {
+        GameObject shot = Instantiate(bullet, transform.position, Quaternion.identity);
+        //shot.transform.Rotate(SpawnPoint.transform.rotation.x, SpawnPoint.transform.rotation.y, SpawnPoint.transform.rotation.z);
+
+    }
+
+ }
