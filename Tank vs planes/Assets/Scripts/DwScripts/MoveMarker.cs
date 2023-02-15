3850352ad2bc79be6d8a55abf07bb47ee66b8720
Andrey Oleshko <Dweren.Iaoeo@mail.ru>
2023-02-15 14:41:41 +0200

    Attention Danger and StopForFightWithBoss

--- Tank vs planes/Assets/Scripts/DwScripts/MoveMarker.cs
+++ Tank vs planes/Assets/Scripts/DwScripts/MoveMarker.cs
@@ -7,19 +7,38 @@ public class MoveMarker : MonoBehaviour
 {
     private float maxValue = 1200;
     private float value;
+    private bool isArrived = false;
+
+    private float valueToShowDangerIcon = 1100;
 
     [SerializeField]
     private float speed;
+
+    public GameObject dangerIcon;
 
     void Start()
     {
         GetComponent<Slider>().maxValue = maxValue;
     }
 
-    
-    void Update()
+    private void FixedUpdate()
     {
         value += speed * Time.deltaTime;
         GetComponent<Slider>().value = value;
+
+        if (value >= maxValue && !isArrived)
+        {
+            isArrived = true;
+            IconsController.Instance.StopMoveEnvironment();
+        }
+
+        if(value >= valueToShowDangerIcon && value < maxValue)
+        {
+            dangerIcon.SetActive(true);
+        } 
+        else
+        {
+            dangerIcon.SetActive(false);
+        }
     }
 }
