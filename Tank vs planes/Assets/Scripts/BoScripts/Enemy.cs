209f88d7f97e74687eec8530ba8e24ca3ed1bd4a
H7H5 <Serdyk87@ukr.net>
2023-02-20 17:58:11 +0200

    start 3d

--- Tank vs planes/Assets/Scripts/BoScripts/Enemy.cs
+++ Tank vs planes/Assets/Scripts/BoScripts/Enemy.cs
@@ -6,30 +6,32 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosion;
     private Vector3 vector;
-    private float movementSpeed = 0f;
+    private float movementSpeed = 10f;
     [SerializeField] private int points = 0;
+    private bool direction = true;
     private void Awake()
     {
-        Init(true);
+        Init(direction);
     }
     public void Init(bool direction)
     {
         movementSpeed = Random.Range(4.0f, 10.0f);
-        if (direction)
-        {
-            vector = Vector3.right;
-        }
-        else
-        {
-            gameObject.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-            vector = Vector3.left;
-        }
-       
+        this.direction = direction;
+
     }
     void Update()
     {
-        transform.Translate(vector * Time.deltaTime * movementSpeed);
+        if (direction)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(1f, 0f, 0f) * movementSpeed;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-1f, 0f, 0f) * movementSpeed;
+        }
+
     }
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("border"))
